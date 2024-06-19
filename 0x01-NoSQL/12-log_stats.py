73a6bb84f812d8b202395d3540a069a54c6363b6
BukeeRuth <olatunjibukola018@gmail.com>
2024-06-19 16:24:37 +0000

    nginx in mongo

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" script that provides some stats about Nginx logs stored in MongoDB """
+from pymongo import MongoClient
+
+def log_stats():
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client.logs
+    collection = db.nginx
+
+    total_logs = collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {method: collection.count_documents({"method": method}) for method in methods}
+    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {method_counts[method]}")
+    print(f"{status_check_count} status check")
